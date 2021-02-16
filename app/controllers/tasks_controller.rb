36c35cdc7117c77d6f8c36ba6ca729e2a9e9c446
Deborah Sillmann <deborahsillmann@gmx.de>
2021-02-16 16:34:46 +0100

    refactor

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def new
@@ -19,22 +21,26 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.update(params_task)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
 
   private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def params_task
     params.require(:task).permit(:title, :details, :completed)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # # 1 view all
+  # get 'tasks', to: 'tasks#index'
 
-  # 1 view all
-  get 'tasks', to: 'tasks#index'
+  # # 2 create new task
+  # # GET the form
+  # get 'tasks/new', to: 'tasks#new'
+  # # POST the input
+  # post 'tasks', to: 'tasks#create'
 
-  # 2 create new task
-  # GET the form
-  get 'tasks/new', to: 'tasks#new'
-  # POST the input
-  post 'tasks', to: 'tasks#create'
+  # # 3 view one
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # 3 view one
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # 4 edit a task
+  # # GET the form (pre-filled) for editing
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # # PATCH the parameters (update what was changes only)
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # 4 edit a task
-  # GET the form (pre-filled) for editing
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-  # PATCH the parameters (update what was changes only)
-  patch 'tasks/:id', to: 'tasks#update'
-
-  # 5 delete a task
-  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
+  # # 5 delete a task
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
 end
