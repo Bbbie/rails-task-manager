d23db5e355f3653ce797231e32017cd7ae7c959d
Deborah Sillmann <deborahsillmann@gmx.de>
2021-02-16 14:07:37 +0100

    create method to create new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,24 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    # create new, empty instance of task to fill in form
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params_task)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+  def params_task
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # 1 view all
   get 'tasks', to: 'tasks#index'
+
+  # 2 create new task
+  # get the form
+  get 'tasks/new', to: 'tasks#new'
+  # post the input
+  post 'tasks', to: 'tasks#create'
+
+  # 3 view one
+  get 'task/:id', to: 'tasks#show', as: :task
+
+
+
 end
